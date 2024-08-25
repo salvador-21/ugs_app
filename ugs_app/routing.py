b63f62cc18c8b649716b0d369f1276d9cc12b14e
salvador-21 <127980837+salvador-21@users.noreply.github.com>
2024-08-25 21:06:21 +0800

    Update routing.py

--- ugs_app/routing.py
+++ ugs_app/routing.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path,re_path
 from .consumers import WSArena
 
 ws_urlpatterns =[
-    path('ws/arena/<str:game_id>',WSArena.as_asgi())
+    # path('ws/arena/<str:game_id>',WSArena.as_asgi())
+    re_path(r'^ws/arena/<str:game_id>',WSArena.as_asgi())
 ]
